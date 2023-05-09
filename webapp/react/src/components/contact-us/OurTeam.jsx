dc58b1b7bd0475627392f9289bfe7a282d0fc6a7
Wimukti <wimukthi.18@cse.mrt.ac.lk>
2023-05-09 13:58:42 +0530

    Add pdf opacity

--- webapp/react/src/components/contact-us/OurTeam.jsx
+++ webapp/react/src/components/contact-us/OurTeam.jsx
@@ -5,21 +5,35 @@ import TeamMember from "./TeamMember";
 
 
 export default function Overview() {
+    const supervisors = [
+        {name:'Prof. Dulani Meedeniya' ,email:'dulani@chestxpert.live', img:'/ourteam/dulani.JPG'},
+        {name:'Dr. Thanuja Ambegoda',email:'thanuja@chestxpert.live' ,img:'/ourteam/thanuja.JPG'},
+    ]
     const teamMembers = [
         {name:'Wimukthi Indeewara' ,email:'wimukthi@chestxpert.live', img:'/ourteam/wimukthi.JPG'},
         {name:'Mahela Hennayake',email:'mahela@chestxpert.live' ,img:'/ourteam/mahela.JPG'},
-        {name:'Kasun Rathnayake' ,email:'kasun@chestxpert.live' ,img:'/ourteam/kasun.JPG'},
-        {name:'Sahan Samarakoon', email:'sahan@chestxpert.live' ,img:'/ourteam/sahan.JPG'}
+        {name:'Kasun Rathnayake' ,email:'kasun@chestxpert.live' ,img:'/ourteam/kasun.JPG'}
     ]
     return (
-        <div className='section-brown' style={{ background: 'url(/bg-pattern.png), linear-gradient(to left, #c52a25, #8a191d)',color:'white'}}>
+        <div className='section-brown'>
             <div className='section' style={{padding:'50px 0px'}}>
                 <div className='section-h2' style={{textAlign:'center', marginBottom:50, marginTop:0, fontSize: 35, fontWeight:'bold'}}>Our Team</div>
+                <div style={{marginBottom: '50px'}}>
+                    <Grid container spacing={5} justifyContent={'center'}>
+                        {
+                            supervisors.map(el=>
+                            <Grid item xs={12} md={4} textAlign={'center'}>
+                                <TeamMember name={el.name} email={el.email} img={el.img}/>
+                            </Grid>
+                            )
+                        }
+                    </Grid>
+                </div>
                 <div>
                     <Grid container spacing={5} justifyContent={'center'}>
                         {
                             teamMembers.map(el=>
-                            <Grid item xs={12} md={5} textAlign={'center'}> 
+                            <Grid item xs={12} md={4} textAlign={'center'}>
                                 <TeamMember name={el.name} email={el.email} img={el.img}/>
                             </Grid>
                             )
